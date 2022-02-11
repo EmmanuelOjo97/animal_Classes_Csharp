fb17fe9b15a0046de7ca320a078677fd3c3dd301
EmmanuelOjo97 <eojo1997@gmail.com>
2022-02-11 09:02:36 +0000

    list added

--- learning_opp_animals/Dog.cs
+++ learning_opp_animals/Dog.cs
@@ -14,42 +14,44 @@ namespace learning_opp_animals
         public double Weight { get; set; }
 
 
-
-
-
-
+        // all handles all 4 attributes
         public Dog(String Name, int Age, String Breed, double Weight) : base(Name, Age)
         {
             this.Breed = Breed;
             this.Weight = Weight;
         }
 
+
+        // handles missing name
         public Dog(int Age, String Breed, double Weight) : this("", Age, Breed, Weight)
         {
 
         }
-
+        // handles missing age
         public Dog(String Name, String Breed, double Weight) : this(Name, 0, Breed, Weight)
         {
 
         }
+        // handles missing breed
         public Dog(String Name, int Age, double Weight) : this(Name, Age, "", Weight)
         {
 
         }
+
+        // handles missing weight
         public Dog(String Name, int Age, String Breed) : this(Name, Age, Breed, 0)
         {
 
         }
-
-        public Dog(String Breed, int Weight) : this("Place Holder Name ", 0, Breed, Weight)
+        // handles missing breed and weight
+        public Dog(String Name, int Age) : this(Name, Age, "Enter Breed", 0)
         {
 
         }
 
-        // single input
 
-        public Dog(String Name) : this(Name, 0, " No dog Breed entered ", 0)
+
+        /*public Dog(int Age, String Breed) : this(Age, Breed, 30)
         {
 
         }
@@ -57,14 +59,7 @@ namespace learning_opp_animals
         public Dog(int Age) : this("Enter Name", Age, " No dog Breed entered ", 0)
         {
 
-        }
-
-
-
-
-
-
-
+        } */
 
 
         public override void eat(string food)
@@ -87,6 +82,27 @@ namespace learning_opp_animals
         public void takeMedication(string meds)
         {
             Console.WriteLine("We have given the dog the " + meds);
+        }
+
+        public string DogHealth(int AgeOfDog)
+        {
+            if (AgeOfDog <= 5)
+            {
+                string message = "The dog is still relatively young";
+                return message;
+                //Console.WriteLine("The dog is still relatively young");
+            }
+            else if (AgeOfDog > 6)
+            {
+                string message = "The dog is getting on in age and will require surgery";
+                return message;
+                //Console.WriteLine("The dog is getting on in age and will require surgery");
+            }
+            else
+            {
+                return "Enter regular age";
+            }
+            // Console.WriteLine("DogHealth " + AgeOfDog);
         }
 
 
--- learning_opp_animals/Program.cs
+++ learning_opp_animals/Program.cs
@@ -10,8 +10,7 @@ public class Program
 
         Bat myBat = new Bat("BatMan", 20);
 
-        //Console.WriteLine(myBat.takeOff);
-        //Console.WriteLine(myBat.flight);
+
         myBat.takeOff();
         myBat.flight();
         myBat.landing();
@@ -19,21 +18,47 @@ public class Program
 
 
         Console.WriteLine(myBat.Age);
-        //myBat.Name = "Not John Cena";
         Console.WriteLine(myBat.Name);
         myBat.eat("french fries");
 
         Console.WriteLine("What is the name of your dog");
         Dog myDog = new Dog("Dogman", 10, "Pug", 30);
-        // myDog.Name = Console.ReadLine();
         Console.WriteLine("Hello there " + myDog.Name + " you are " + myDog.Age + " years old");
         Console.WriteLine(myDog.Breed);
 
-        Dog JohnnyTheDog = new Dog("John", 2, "Pug", 1);
+        Dog JohnnyTheDog = new Dog("Johnny", 10, "Pug", 25);
         Console.WriteLine(JohnnyTheDog.Name + JohnnyTheDog.Age + JohnnyTheDog.Breed + JohnnyTheDog.Weight);
 
         Snake SnapeTheSnake = new Snake("Snape", 50, "Python", 5.6);
         Console.WriteLine(SnapeTheSnake.Species);
+        Console.WriteLine(myDog.DogHealth(30));
+        Console.WriteLine(myDog.Age);
+
+        Animal myExtraAnimal = new Bat("ManBat", 4);
+        LinkedList<Animal> zoo = new LinkedList<Animal>();
+        List<Animal> list = new List<Animal>();
+        list.Add(myBat);
+        list.Add(myExtraAnimal);
+        list.Add(myDog);
+        list.Add(JohnnyTheDog);
+        list.Add(SnapeTheSnake);
+
+
+        //myBat.Name = Console.ReadLine();
+        Console.WriteLine(myBat.Name);
+
+        foreach (Animal a in list)
+        {
+            Console.WriteLine(a.Name);
+            Console.WriteLine(a.Age);
+        }
+
+        bool runProgram = true;
+
+
+
+
+
 
 
     }
